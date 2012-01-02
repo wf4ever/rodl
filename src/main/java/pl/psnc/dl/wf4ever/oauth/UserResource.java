d6b14fe06874ef2754bd3fff75cf283921326f08
Piotr Hołubowicz <piotrhol@man.poznan.pl>
2012-01-02 10:07:03 +0100

    Fixes WFE-141.

--- src/main/java/pl/psnc/dl/wf4ever/oauth/UserResource.java
+++ src/main/java/pl/psnc/dl/wf4ever/oauth/UserResource.java
@@ -1,10 +1,16 @@
 package pl.psnc.dl.wf4ever.oauth;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.UnknownHostException;
 import java.rmi.RemoteException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
+import javax.naming.NamingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -15,17 +21,20 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
 
 import org.apache.commons.codec.binary.Base64;
 
 import pl.psnc.dl.wf4ever.Constants;
 import pl.psnc.dl.wf4ever.auth.OAuthManager;
 import pl.psnc.dl.wf4ever.connection.DigitalLibraryFactory;
+import pl.psnc.dl.wf4ever.connection.SemanticMetadataServiceFactory;
 import pl.psnc.dl.wf4ever.dlibra.ConflictException;
 import pl.psnc.dl.wf4ever.dlibra.DigitalLibrary;
 import pl.psnc.dl.wf4ever.dlibra.DigitalLibraryException;
 import pl.psnc.dl.wf4ever.dlibra.NotFoundException;
 import pl.psnc.dl.wf4ever.dlibra.UserProfile;
+import pl.psnc.dl.wf4ever.sms.SemanticMetadataService;
 
 /**
  * 
@@ -38,6 +47,9 @@ public class UserResource
 
 	@Context
 	HttpServletRequest request;
+
+	@Context
+	UriInfo uriInfo;
 
 
 	@GET
@@ -105,24 +117,43 @@ public class UserResource
 	/**
 	 * Deletes the workspace.
 	 * @param userId identifier of a workspace in the RO SRS
-	 * @throws RemoteException
-	 * @throws UnknownHostException 
-	 * @throws MalformedURLException 
 	 * @throws DigitalLibraryException 
 	 * @throws NotFoundException 
+	 * @throws SQLException 
+	 * @throws NamingException 
+	 * @throws IOException 
+	 * @throws ClassNotFoundException 
 	 */
 	@DELETE
 	public void deleteUser(@PathParam("U_ID")
 	String urlSafeUserId)
-		throws RemoteException, MalformedURLException, UnknownHostException,
-		DigitalLibraryException, NotFoundException
+		throws DigitalLibraryException, NotFoundException,
+		ClassNotFoundException, IOException, NamingException, SQLException
 	{
 		UserProfile user = (UserProfile) request.getAttribute(Constants.USER);
 		OAuthManager oauth = new OAuthManager();
 		DigitalLibrary dl = DigitalLibraryFactory.getDigitalLibrary(
 			user.getLogin(), user.getPassword());
+		SemanticMetadataService sms = SemanticMetadataServiceFactory
+				.getService(user);
 
 		String userId = new String(Base64.decodeBase64(urlSafeUserId));
+
+		List<String> list = dl.getWorkspaceIds();
+		try {
+			for (String workspaceId : list) {
+				dl.deleteWorkspace(workspaceId);
+				Set<URI> versions = sms.findResearchObjects(uriInfo
+						.getBaseUriBuilder().path("workspaces")
+						.path(workspaceId).build());
+				for (URI uri : versions) {
+					sms.removeResearchObject(uri);
+				}
+			}
+		}
+		finally {
+			sms.close();
+		}
 
 		dl.deleteUser(userId);
 		oauth.deleteUserCredentials(userId);
