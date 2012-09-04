f77f272847b94a8fdc06f20ffb059997e5242a39
Piotr Hołubowicz <piotrhol@man.poznan.pl>
2012-09-04 14:20:18 +0200

    Fixes WFE-620.

--- src/main/java/pl/psnc/dl/wf4ever/Constants.java
+++ src/main/java/pl/psnc/dl/wf4ever/Constants.java
@@ -46,7 +46,7 @@ public final class Constants {
     public static final String LINK_HEADER_TEMPLATE = "<%s>; rel=\"%s\"";
 
     public static final Pattern AO_LINK_HEADER_PATTERN = Pattern
-            .compile("<(.+)>; rel=\"http://purl.org/ao/annotates\"");
+            .compile("\\s*<([^>]*)>\\s*;\\s*rel\\s*=\\s*\"http://purl.org/ao/annotatesResource\"");
 
     public static final String AO_ANNOTATION_BODY_HEADER = "http://purl.org/ao/body";
 
