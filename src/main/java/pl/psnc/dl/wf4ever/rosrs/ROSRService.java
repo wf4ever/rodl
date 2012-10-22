d5a9caa480ab412f1da6ab5893c540630926bce3
Piotr Hołubowicz <piotrhol@man.poznan.pl>
2012-10-22 09:35:31 +0200

    Fixed removing annotations (refs WFE-709)

--- src/main/java/pl/psnc/dl/wf4ever/rosrs/ROSRService.java
+++ src/main/java/pl/psnc/dl/wf4ever/rosrs/ROSRService.java
@@ -549,12 +549,14 @@ public final class ROSRService {
             throws DigitalLibraryException, NotFoundException, AccessDeniedException {
         String filePath = researchObject.getUri().relativize(resource).getPath();
         InputStream data = ROSRService.DL.get().getFileContents(researchObject, filePath);
-        RDFFormat format = RDFFormat.forMIMEType(ROSRService.DL.get().getFileInfo(researchObject, filePath)
-                .getMimeType());
-        ROSRService.SMS.get().addNamedGraph(resource, data, format);
-        // update the named graph copy in dLibra, the manifest is not changed
-        updateNamedGraphInDlibra(filePath, researchObject, resource);
-        updateROAttributesInDlibra(researchObject);
+        if (data != null) {
+            RDFFormat format = RDFFormat.forMIMEType(ROSRService.DL.get().getFileInfo(researchObject, filePath)
+                    .getMimeType());
+            ROSRService.SMS.get().addNamedGraph(resource, data, format);
+            // update the named graph copy in dLibra, the manifest is not changed
+            updateNamedGraphInDlibra(filePath, researchObject, resource);
+            updateROAttributesInDlibra(researchObject);
+        }
     }
 
 
@@ -568,7 +570,7 @@ public final class ROSRService {
      *            URI of the resource that is converted
      */
     public static void convertAnnotationBodyToAggregatedResource(ResearchObject researchObject, URI resource) {
-        if (ROSRService.SMS.get().isROMetadataNamedGraph(researchObject, resource)) {
+        if (ROSRService.SMS.get().containsNamedGraph(resource)) {
             ROSRService.SMS.get().removeNamedGraph(researchObject, resource);
             updateROAttributesInDlibra(researchObject);
         }
