fa3ec60bc52da5f2c9e4662bacbb39d1a9f2fb59
pejot <pejot@pejot-pcsn-machine.(none)>
2012-12-06 13:56:48 +0100

    slimming down MemoryZipFile

--- src/main/java/pl/psnc/dl/wf4ever/utils/zip/MemoryZipFile.java
+++ src/main/java/pl/psnc/dl/wf4ever/utils/zip/MemoryZipFile.java
@@ -1,13 +1,10 @@
 package pl.psnc.dl.wf4ever.utils.zip;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
-import java.util.zip.ZipOutputStream;
 
 import org.apache.commons.io.IOUtils;
 
@@ -19,21 +16,7 @@ import org.apache.commons.io.IOUtils;
  */
 public class MemoryZipFile {
 
-    /** Output of loaded zip file. */
-    private ZipOutputStream zipOutputStream;
-    /** Byte array necessary to create an output stream. */
-    private ByteArrayOutputStream byteArrayOutputStream;
-    /** Read/Created zip file. */
     private ZipFile zipFile;
-
-
-    /**
-     * Constructor.
-     */
-    public MemoryZipFile() {
-        byteArrayOutputStream = new ByteArrayOutputStream();
-        zipOutputStream = new ZipOutputStream(byteArrayOutputStream);
-    }
 
 
     /**
@@ -52,41 +35,6 @@ public class MemoryZipFile {
 
     public ZipFile getZipFile() {
         return zipFile;
-    }
-
-
-    /**
-     * Create a new file as a zipEntry.
-     * 
-     * @param fileName
-     *            entry name
-     * @param data
-     *            file content
-     * @throws IOException .
-     */
-    public void addEntry(String fileName, byte[] data)
-            throws IOException {
-        zipOutputStream.putNextEntry(new ZipEntry(fileName));
-        zipOutputStream.write(data);
-        zipOutputStream.closeEntry();
-    }
-
-
-    /**
-     * Create a new directory as a zipEntry.
-     * 
-     * @param dirName
-     *            directory name
-     * @throws IOException .
-     */
-    public void addEntry(String dirName)
-            throws IOException {
-        zipOutputStream.putNextEntry(new ZipEntry(dirName));
-    }
-
-
-    public byte[] getZipAsBytesArray() {
-        return byteArrayOutputStream.toByteArray();
     }
 
 
