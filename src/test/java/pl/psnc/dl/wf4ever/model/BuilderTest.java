8df466cd54d34a34f1b13dad5e9d8d24bb6cb6b2
pejot <pejot@pejot-pcsn-machine.(none)>
2013-02-20 12:48:23 +0100

    builder tests

--- src/test/java/pl/psnc/dl/wf4ever/model/BuilderTest.java
+++ src/test/java/pl/psnc/dl/wf4ever/model/BuilderTest.java
@@ -2,19 +2,26 @@ package pl.psnc.dl.wf4ever.model;
 
 import java.net.URI;
 import java.util.HashSet;
+import java.util.Set;
 
 import junit.framework.Assert;
 
 import org.joda.time.DateTime;
+import org.junit.Before;
 import org.junit.Test;
 
 import pl.psnc.dl.wf4ever.dl.UserMetadata;
 import pl.psnc.dl.wf4ever.dl.UserMetadata.Role;
+import pl.psnc.dl.wf4ever.model.AO.Annotation;
 import pl.psnc.dl.wf4ever.model.ORE.AggregatedResource;
 import pl.psnc.dl.wf4ever.model.ORE.Proxy;
 import pl.psnc.dl.wf4ever.model.RDF.Thing;
+import pl.psnc.dl.wf4ever.model.RO.Folder;
+import pl.psnc.dl.wf4ever.model.RO.FolderEntry;
+import pl.psnc.dl.wf4ever.model.RO.FolderResourceMap;
 import pl.psnc.dl.wf4ever.model.RO.Manifest;
 import pl.psnc.dl.wf4ever.model.RO.ResearchObject;
+import pl.psnc.dl.wf4ever.model.RO.Resource;
 import pl.psnc.dl.wf4ever.model.ROEVO.SnapshotResearchObject;
 
 import com.hp.hpl.jena.query.Dataset;
@@ -33,6 +40,17 @@ public class BuilderTest {
     private Boolean useTransaction = false;
     Builder builder = new Builder(user, dataset, useTransaction);
     URI exampleUri = URI.create("http://www.example.com/exampleUri/");
+    ResearchObject researchObject;
+    DateTime now;
+    UserMetadata userM;
+
+
+    @Before
+    public void setUp() {
+        researchObject = builder.buildResearchObject(exampleUri.resolve("research-object"));
+        DateTime.now();
+        userM = new UserMetadata("janek", "Jane Kowalski", Role.AUTHENTICATED);
+    }
 
 
     @Test
@@ -47,7 +65,6 @@ public class BuilderTest {
         Thing thing = builder.buildThing(exampleUri);
         Assert.assertEquals(thing.getBuilder(), builder);
         Assert.assertEquals(thing.getUri(), exampleUri);
-        Assert.assertEquals(thing.getContributors(), new HashSet());
         Assert.assertNotNull(thing.getFilename());
         Assert.assertNull(thing.getArchivedAt());
         Assert.assertNull(thing.getArchivedBy());
@@ -69,7 +86,6 @@ public class BuilderTest {
 
     @Test
     public void testBuildSnapshotResearchObject() {
-        ResearchObject researchObject = builder.buildResearchObject(exampleUri);
         SnapshotResearchObject snapshotResearchObject = builder.buildSnapshotResearchObject(
             exampleUri.resolve("snapshot"), researchObject);
         Assert.assertNull(snapshotResearchObject.getSnapshottedAt());
@@ -79,7 +95,6 @@ public class BuilderTest {
 
     @Test
     public void testBuildArchiveResearchObject() {
-        ResearchObject researchObject = builder.buildResearchObject(exampleUri);
         SnapshotResearchObject snapshotResearchObject = builder.buildSnapshotResearchObject(
             exampleUri.resolve("archive"), researchObject);
         Assert.assertNull(snapshotResearchObject.getArchivedAt());
@@ -89,7 +104,6 @@ public class BuilderTest {
 
     @Test
     public void testBuildManifest() {
-        ResearchObject researchObject = builder.buildResearchObject(exampleUri.resolve("research-object"));
         Manifest manifest = builder.buildManifest(exampleUri, researchObject);
         Assert.assertEquals(manifest.getResearchObject(), researchObject);
     }
@@ -97,8 +111,6 @@ public class BuilderTest {
 
     @Test
     public void testBuildManifest2() {
-        ResearchObject researchObject = builder.buildResearchObject(exampleUri.resolve("research-object"));
-        DateTime now = DateTime.now();
         Manifest manifest = builder.buildManifest(exampleUri, researchObject, user, now);
         Assert.assertEquals(manifest.getResearchObject(), researchObject);
         Assert.assertEquals(manifest.getCreator(), user);
@@ -108,8 +120,6 @@ public class BuilderTest {
 
     @Test
     public void testBuildAggregatedResource() {
-        ResearchObject researchObject = builder.buildResearchObject(exampleUri);
-        DateTime now = DateTime.now();
         AggregatedResource aggregatedResource = builder.buildAggregatedResource(
             exampleUri.resolve("aggregated-resource"), researchObject, user, now);
         Assert.assertEquals(aggregatedResource.getCreated(), now);
@@ -120,7 +130,6 @@ public class BuilderTest {
 
     @Test
     public void testBuildProxy() {
-        ResearchObject researchObject = builder.buildResearchObject(exampleUri.resolve("research-object"));
         AggregatedResource aggregatedResource = builder.buildAggregatedResource(
             exampleUri.resolve("aggregated-resource"), researchObject, user, DateTime.now());
         Proxy proxy = builder.buildProxy(exampleUri, aggregatedResource, researchObject);
@@ -129,8 +138,76 @@ public class BuilderTest {
     }
 
 
+    @Test
     public void testBuildAnnotation() {
-
+        Thing body = builder.buildThing(exampleUri.resolve("body"));
+        Set<Thing> targets = new HashSet<Thing>();
+        targets.add(researchObject);
+        Annotation annotation = builder.buildAnnotation(researchObject, exampleUri, body, targets);
+        Assert.assertEquals(annotation.getResearchObject(), researchObject);
+        Assert.assertEquals(annotation.getBody(), body);
     }
 
+
+    @Test
+    public void testBuildAnnotation2() {
+        Thing body = builder.buildThing(exampleUri.resolve("body"));
+        Set<Thing> targets = new HashSet<Thing>();
+        targets.add(researchObject);
+        Annotation annotation = builder.buildAnnotation(researchObject, exampleUri, body, targets, userM, now);
+        Assert.assertEquals(annotation.getResearchObject(), researchObject);
+        Assert.assertEquals(annotation.getBody(), body);
+        Assert.assertEquals(annotation.getCreator(), userM);
+        Assert.assertEquals(annotation.getCreated(), now);
+    }
+
+
+    @Test
+    public void testBuildAnnotation3() {
+        Thing body = builder.buildThing(exampleUri.resolve("body"));
+        Annotation annotation = builder.buildAnnotation(researchObject, exampleUri, body, researchObject, userM, now);
+        Assert.assertEquals(annotation.getResearchObject(), researchObject);
+        Assert.assertEquals(annotation.getBody(), body);
+        Assert.assertEquals(annotation.getCreator(), userM);
+        Assert.assertEquals(annotation.getCreated(), now);
+    }
+
+
+    @Test
+    public void testBuildResource() {
+        Resource resource = builder.buildResource(researchObject, exampleUri, userM, now);
+        Assert.assertEquals(resource.getResearchObject(), researchObject);
+        Assert.assertEquals(resource.getCreator(), userM);
+        Assert.assertEquals(resource.getCreated(), now);
+    }
+
+
+    @Test
+    public void testBuildFolder() {
+        Folder folder = builder.buildFolder(researchObject, exampleUri, userM, now);
+        Assert.assertEquals(folder.getResearchObject(), researchObject);
+        Assert.assertEquals(folder.getCreator(), userM);
+        Assert.assertEquals(folder.getCreated(), now);
+    }
+
+
+    @Test
+    public void testBuildFolderResourceMap() {
+        Folder folder = builder.buildFolder(researchObject, exampleUri.resolve("folder"), userM, now);
+        FolderResourceMap folderResourceMap = builder.buildFolderResourceMap(exampleUri, folder);
+        Assert.assertEquals(folderResourceMap.getFolder(), folder);
+    }
+
+
+    @Test
+    public void testBuildFolderEntry() {
+        AggregatedResource aggregatedResource = builder.buildAggregatedResource(
+            exampleUri.resolve("aggregated-resource"), researchObject, user, now);
+        Folder folder = builder.buildFolder(researchObject, exampleUri.resolve("folder"), userM, now);
+        FolderEntry folderEntry = builder.buildFolderEntry(exampleUri, aggregatedResource, folder, "entry");
+        Assert.assertEquals(folderEntry.getFilename(), exampleUri.getPath().replaceAll("/", ""));
+        Assert.assertEquals(folderEntry.getEntryName(), "entry");
+        Assert.assertEquals(folderEntry.getFolder(), folder);
+        Assert.assertEquals(folderEntry.getProxyIn(), folder);
+    }
 }
