ebfbad55e4413c026515d1ba2930c68b9cb30713
pejot <pejot@pejot-pcsn-machine.(none)>
2013-02-22 16:54:08 +0100

    ResourceMap saveTest implemented

--- src/test/java/pl/psnc/dl/wf4ever/model/ORE/ResourceMapTest.java
+++ src/test/java/pl/psnc/dl/wf4ever/model/ORE/ResourceMapTest.java
@@ -6,9 +6,17 @@ import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.openrdf.rio.RDFFormat;
 
 import pl.psnc.dl.wf4ever.model.BaseTest;
 import pl.psnc.dl.wf4ever.model.RO.ResearchObject;
+import pl.psnc.dl.wf4ever.vocabulary.ORE;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.DCTerms;
+import com.hp.hpl.jena.vocabulary.RDF;
 
 public class ResourceMapTest extends BaseTest {
 
@@ -48,8 +56,22 @@ public class ResourceMapTest extends BaseTest {
 
     @Test
     public void testSave() {
-        //TODO WHY ??
         resourceMap.save();
-    }
+        Model model = ModelFactory.createDefaultModel();
+        model.read(resourceMap.getGraphAsInputStream(RDFFormat.RDFXML), null);
 
+        Resource r = model.getResource(researchObject.getUri().toString());
+        Assert.assertNotNull(r);
+        Assert.assertNotNull(r.getProperty(DCTerms.creator));
+        Assert.assertNotNull(r.getProperty(DCTerms.created));
+        Assert.assertEquals(resourceMapUri.toString(), r.getPropertyResourceValue(ORE.isDescribedBy).getURI()
+                .toString());
+
+        r = model.getResource(resourceMapUri.toString());
+        Assert.assertNotNull(r);
+        Assert.assertEquals(researchObject.getUri().toString(), r.getPropertyResourceValue(ORE.describes).getURI()
+                .toString());
+        Assert.assertEquals(ORE.ResourceMap.getURI(), r.getPropertyResourceValue(RDF.type).getURI().toString());
+
+    }
 }
