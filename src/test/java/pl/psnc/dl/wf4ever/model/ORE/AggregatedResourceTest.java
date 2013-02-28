034a37f4c0975a6008089444105aeb4906b2d794
Piotr Hołubowicz <piotrhol@man.poznan.pl>
2013-02-28 14:36:52 +0100

    Remove unnecessary printing.

--- src/test/java/pl/psnc/dl/wf4ever/model/ORE/AggregatedResourceTest.java
+++ src/test/java/pl/psnc/dl/wf4ever/model/ORE/AggregatedResourceTest.java
@@ -143,8 +143,6 @@ public class AggregatedResourceTest extends BaseTest {
         is.close();
         Model result = ModelFactory.createDefaultModel();
         result.read(aggregatedResource.getGraphAsInputStream(RDFFormat.RDFXML), "");
-        expected.write(System.out, "TURTLE");
-        result.write(System.out, "TURTLE");
         Assert.assertTrue(expected.isIsomorphicWith(result));
     }
 }
