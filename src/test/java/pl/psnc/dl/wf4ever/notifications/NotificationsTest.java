862a41eba974ff4be61767f04b9ae5e0b4b37307
pejot <pejot@pejot-pcsn-machine.(none)>
2013-05-07 11:25:35 +0200

    NotificationsTest@testNoficationSource test fixed

--- src/test/java/pl/psnc/dl/wf4ever/notifications/NotificationsTest.java
+++ src/test/java/pl/psnc/dl/wf4ever/notifications/NotificationsTest.java
@@ -11,7 +11,6 @@ import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-import pl.psnc.dl.wf4ever.ApplicationProperties;
 import pl.psnc.dl.wf4ever.IntegrationTest;
 import pl.psnc.dl.wf4ever.W4ETest;
 
@@ -94,7 +93,8 @@ public class NotificationsTest extends W4ETest {
     public void testNotificationSource()
             throws IllegalArgumentException, MalformedURLException, FeedException, IOException, URISyntaxException {
         ro = createRO(accessToken);
-        URI expectedUri = new URI(ro.getScheme(), ro.getAuthority(), ApplicationProperties.getContextPath());
+        System.out.println(ro.getHost());
+        System.out.println(ro.getScheme());
         SyndFeedInput input = new SyndFeedInput();
         SyndFeed feed = input.build(new XmlReader(webResource.path("notifications/").queryParam("ro", ro.toString())
                 .getURI().toURL()));
@@ -103,7 +103,8 @@ public class NotificationsTest extends W4ETest {
             for (Object obLink : entry.getLinks()) {
                 SyndLink link = (SyndLink) obLink;
                 if (link != null && link.getRel().equals(DCTerms.source.toString())) {
-                    Assert.assertEquals(expectedUri.toString(), link.getHref());
+                    //the source is this producer host
+                    Assert.assertTrue(link.getHref().contains(ro.getHost()));
                 }
             }
         }
