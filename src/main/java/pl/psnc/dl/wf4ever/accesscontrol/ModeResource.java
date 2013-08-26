6adf8a515ecd313f3b7a242a0f5e72d8d63c9006
pejot <pejot@pejot-pcsn-machine.(none)>
2013-08-26 11:48:23 +0200

    Mode and Permission Resources scaffold implemented

--- src/main/java/pl/psnc/dl/wf4ever/accesscontrol/ModeResource.java
+++ src/main/java/pl/psnc/dl/wf4ever/accesscontrol/ModeResource.java
@@ -1,6 +1,20 @@
 package pl.psnc.dl.wf4ever.accesscontrol;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.apache.log4j.Logger;
+
+import pl.psnc.dl.wf4ever.auth.RequestAttribute;
+import pl.psnc.dl.wf4ever.model.Builder;
 
 /**
  * API for setting Research Object access mode.
@@ -10,5 +24,38 @@ import javax.ws.rs.Path;
  */
 @Path("accesscontrol/modes/")
 public class ModeResource {
+
+    /** logger. */
+    private static final Logger LOGGER = Logger.getLogger(ModeResource.class);
+
+    /** URI info. */
+    @Context
+    UriInfo uriInfo;
+
+    /** Resource builder. */
+    @RequestAttribute("Builder")
+    private Builder builder;
+
+
+    @POST
+    @Consumes("application/json")
+    public Response setMode() {
+        return null;
+    }
+
+
+    @Path("{mode_id}/")
+    @Produces("application/json")
+    @GET
+    public Response getMoge(@PathParam("mode_id") String mode_id) {
+        return null;
+    }
+
+
+    @Produces("application/json")
+    @GET
+    public Response getMoges(@QueryParam("ro") String ro) {
+        return null;
+    }
 
 }
