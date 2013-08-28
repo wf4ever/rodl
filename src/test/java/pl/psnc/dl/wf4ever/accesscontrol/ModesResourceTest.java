62b8b9957425ab484dd3c01c670930f9044378da
pejot <pejot@pejot-pcsn-machine.(none)>
2013-08-28 14:56:35 +0200

    Mode json conversion problem UNsolved

--- src/test/java/pl/psnc/dl/wf4ever/accesscontrol/ModesResourceTest.java
+++ src/test/java/pl/psnc/dl/wf4ever/accesscontrol/ModesResourceTest.java
@@ -1,7 +1,10 @@
 package pl.psnc.dl.wf4ever.accesscontrol;
 
+import java.net.URI;
+
 import javax.ws.rs.core.MediaType;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -14,7 +17,7 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource.Builder;
 
 @Category(IntegrationTest.class)
-public class ModeResourceTest extends AbstractIntegrationTest {
+public class ModesResourceTest extends AbstractIntegrationTest {
 
     @Override
     @Before
@@ -25,7 +28,7 @@ public class ModeResourceTest extends AbstractIntegrationTest {
 
 
     @Override
-    @Before
+    @After
     public void tearDown()
             throws Exception {
         super.tearDown();
@@ -34,6 +37,22 @@ public class ModeResourceTest extends AbstractIntegrationTest {
 
     @Test
     public void testIfModeIsSetOnceTheROisCreated() {
+        URI createdRO = createRO();
+        Mode mode = webResource.path("accesscontrol/modes/").queryParam("ro", createdRO.toString())
+                .header("Authorization", "Bearer " + adminCreds).get(Mode.class);
+
+        System.out.println("*******************");
+        System.out.println("*******************");
+        System.out.println("*******************");
+        System.out.println("*******************");
+        System.out.println(mode.getMode());
+        System.out.println(mode.getId());
+        System.out.println(mode.getRo());
+        System.out.println("*******************");
+        System.out.println("*******************");
+        System.out.println("*******************");
+        System.out.println("*******************");
+        delete(createdRO, adminCreds);
 
     }
 
@@ -46,7 +65,7 @@ public class ModeResourceTest extends AbstractIntegrationTest {
 
     @Test
     public void testSinglePost() {
-        Builder builder = webResource.path("evo/copy/").header("Authorization", "Bearer " + accessToken);
+        Builder builder = webResource.path("accesscontrol/modes/").header("Authorization", "Bearer " + accessToken);
         Mode mode = new Mode();
         ClientResponse response = builder.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, mode);
     }
