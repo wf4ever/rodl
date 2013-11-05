8075b9be62aa7da08924f603de0998f823c3c037
filip franek <filip@prostanthera.man.poznan.pl>
2013-11-05 17:35:22 +0100

    AccessControl service description added.:

--- src/test/java/pl/psnc/dl/wf4ever/integration/rosrs/RootResourceTest.java
+++ src/test/java/pl/psnc/dl/wf4ever/integration/rosrs/RootResourceTest.java
@@ -6,6 +6,7 @@ import org.junit.experimental.categories.Category;
 
 import pl.psnc.dl.wf4ever.integration.AbstractIntegrationTest;
 import pl.psnc.dl.wf4ever.integration.IntegrationTest;
+import pl.psnc.dl.wf4ever.vocabulary.AccessControlService;
 import pl.psnc.dl.wf4ever.vocabulary.NotificationService;
 import pl.psnc.dl.wf4ever.vocabulary.ROEVOService;
 
@@ -17,36 +18,47 @@ import com.hp.hpl.jena.rdf.model.RDFNode;
 @Category(IntegrationTest.class)
 public class RootResourceTest extends AbstractIntegrationTest {
 
-    @Test
-    public void testNotificationServiceDescription() {
-        Model model = ModelFactory.createDefaultModel();
-        model.read(resource().getURI().toString());
-        RDFNode val = model.getProperty(model.createResource(resource().getURI().toString()),
-            NotificationService.notifications).getObject();
-        UriTemplate uriTemplate = UriTemplate.fromTemplate(val.toString());
-        uriTemplate.set("ro", "ro-value");
-        uriTemplate.set("from", "from-value");
-        uriTemplate.set("to", "to-value");
-        uriTemplate.set("source", "source-value");
-        uriTemplate.set("limit", "limit-value");
-        Assert.assertTrue(uriTemplate.getValues().containsKey("ro"));
-        Assert.assertTrue(uriTemplate.getValues().containsKey("from"));
-        Assert.assertTrue(uriTemplate.getValues().containsKey("to"));
-        Assert.assertTrue(uriTemplate.getValues().containsKey("source"));
-        Assert.assertTrue(uriTemplate.getValues().containsKey("limit"));
-        Assert.assertFalse(uriTemplate.getValues().containsKey("any-other"));
-    }
+	@Test
+	public void testNotificationServiceDescription() {
+		Model model = ModelFactory.createDefaultModel();
+		model.read(resource().getURI().toString());
+		RDFNode val = model.getProperty(model.createResource(resource().getURI().toString()),
+				NotificationService.notifications).getObject();
+		UriTemplate uriTemplate = UriTemplate.fromTemplate(val.toString());
+		uriTemplate.set("ro", "ro-value");
+		uriTemplate.set("from", "from-value");
+		uriTemplate.set("to", "to-value");
+		uriTemplate.set("source", "source-value");
+		uriTemplate.set("limit", "limit-value");
+		Assert.assertTrue(uriTemplate.getValues().containsKey("ro"));
+		Assert.assertTrue(uriTemplate.getValues().containsKey("from"));
+		Assert.assertTrue(uriTemplate.getValues().containsKey("to"));
+		Assert.assertTrue(uriTemplate.getValues().containsKey("source"));
+		Assert.assertTrue(uriTemplate.getValues().containsKey("limit"));
+		Assert.assertFalse(uriTemplate.getValues().containsKey("any-other"));
+	}
 
+	@Test
+	public void testAccessControlDescription() {
+		Model model = ModelFactory.createDefaultModel();
+		model.read(resource().getURI().toString());
+		RDFNode val = model.getProperty(model.createResource(resource().getURI().toString()),
+				AccessControlService.permissions).getObject();
+		Assert.assertNotNull(val);
+		val = model.getProperty(model.createResource(resource().getURI().toString()),
+				AccessControlService.modes).getObject();
+		Assert.assertNotNull(val);
+	}
 
-    @Test
-    public void testEvolutionServiceDescription() {
-        Model model = ModelFactory.createDefaultModel();
-        model.read(resource().getURI().toString());
-        RDFNode val = model.getProperty(model.createResource(resource().getURI().toString()), ROEVOService.info)
-                .getObject();
-        UriTemplate uriTemplate = UriTemplate.fromTemplate(val.toString());
-        uriTemplate.set("ro", "ro-value");
+	@Test
+	public void testEvolutionServiceDescription() {
+		Model model = ModelFactory.createDefaultModel();
+		model.read(resource().getURI().toString());
+		RDFNode val = model.getProperty(model.createResource(resource().getURI().toString()),
+				ROEVOService.info).getObject();
+		UriTemplate uriTemplate = UriTemplate.fromTemplate(val.toString());
+		uriTemplate.set("ro", "ro-value");
 
-        Assert.assertFalse(uriTemplate.getValues().containsKey("any-other"));
-    }
+		Assert.assertFalse(uriTemplate.getValues().containsKey("any-other"));
+	}
 }
