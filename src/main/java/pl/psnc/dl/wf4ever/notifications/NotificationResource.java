7872c19764cb2a8681260ad62f544bb3793ccb0b
filip franek <filip@prostanthera.man.poznan.pl>
2013-11-08 11:53:06 +0100

    Notifications format updated.

--- src/main/java/pl/psnc/dl/wf4ever/notifications/NotificationResource.java
+++ src/main/java/pl/psnc/dl/wf4ever/notifications/NotificationResource.java
@@ -31,76 +31,76 @@ import com.sun.syndication.io.WireFeedOutput;
  * @author pejot
  * 
  */
-@Path("notifications/")
+@Path("atomnotifications/")
 public class NotificationResource {
 
-    /** Logger. */
-    private static final Logger LOGGER = Logger.getLogger(NotificationResource.class);
+	/** Logger. */
+	private static final Logger LOGGER = Logger.getLogger(NotificationResource.class);
 
+	/**
+	 * Get AtomFeed with list of entries.
+	 * 
+	 * @param uriInfo
+	 *            injected context information
+	 * @param roUri
+	 *            Research Object URI
+	 * @param from
+	 *            time - from
+	 * @param to
+	 *            time - to
+	 * @param source
+	 *            feed field source
+	 * @param limit
+	 *            max number of returned entry
+	 * @return Atom Feed with the list of requested entries.
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_ATOM_XML)
+	public Response getAtomFeeds(@Context UriInfo uriInfo, @QueryParam("ro") URI roUri,
+			@QueryParam("from") String from, @QueryParam("to") String to,
+			@QueryParam("source") URI source, @QueryParam("limit") Integer limit) {
+		AtomFeedEntryDAO entryDAO = new AtomFeedEntryDAO();
+		// title depends on filters
+		Date dateFrom = (from != null) ? DateTime.parse(from).toDate() : null;
+		Date dateTo = (to != null) ? DateTime.parse(to).toDate() : null;
+		String id = uriInfo.getRequestUri().toString();
+		String title = Title.build(roUri, dateFrom, dateTo);
+		List<Notification> entries = entryDAO.find(roUri, dateFrom, dateTo, source, limit);
+		NotificationFeed atomFeed = new NotificationFeed.Builder(id).title(title).entries(entries)
+				.build();
+		Feed feed = atomFeed.asFeed(FeedType.ATOM_1_0, uriInfo.getRequestUri());
+		WireFeedOutput wire = new WireFeedOutput();
+		try {
+			return Response.ok(wire.outputString(feed)).build();
+		} catch (IllegalArgumentException | FeedException e) {
+			LOGGER.error("Cannot parse entries", e);
+			return Response.serverError().build();
+		}
+	}
 
-    /**
-     * Get AtomFeed with list of entries.
-     * 
-     * @param uriInfo
-     *            injected context information
-     * @param roUri
-     *            Research Object URI
-     * @param from
-     *            time - from
-     * @param to
-     *            time - to
-     * @param source
-     *            feed field source
-     * @param limit
-     *            max number of returned entry
-     * @return Atom Feed with the list of requested entries.
-     */
-    @GET
-    @Produces(MediaType.APPLICATION_ATOM_XML)
-    public Response getAtomFeeds(@Context UriInfo uriInfo, @QueryParam("ro") URI roUri,
-            @QueryParam("from") String from, @QueryParam("to") String to, @QueryParam("source") URI source,
-            @QueryParam("limit") Integer limit) {
-        AtomFeedEntryDAO entryDAO = new AtomFeedEntryDAO();
-        //title depends on filters
-        Date dateFrom = (from != null) ? DateTime.parse(from).toDate() : null;
-        Date dateTo = (to != null) ? DateTime.parse(to).toDate() : null;
-        String id = uriInfo.getRequestUri().toString();
-        String title = Title.build(roUri, dateFrom, dateTo);
-        List<Notification> entries = entryDAO.find(roUri, dateFrom, dateTo, source, limit);
-        NotificationFeed atomFeed = new NotificationFeed.Builder(id).title(title).entries(entries).build();
-        Feed feed = atomFeed.asFeed(FeedType.ATOM_1_0, uriInfo.getRequestUri());
-        WireFeedOutput wire = new WireFeedOutput();
-        try {
-            return Response.ok(wire.outputString(feed)).build();
-        } catch (IllegalArgumentException | FeedException e) {
-            LOGGER.error("Cannot parse entries", e);
-            return Response.serverError().build();
-        }
-    }
-
-
-    /**
-     * Delete Atom feed entries.
-     * 
-     * @param roUri
-     *            Research Object uri
-     * @param from
-     *            time - from
-     * @param to
-     *            time - to
-     * @param source
-     *            feed field source
-     * @param limit
-     *            max number of returned entry
-     * @return Atom Feed with the list of requested entrires.
-     */
-    @DELETE
-    public Response deleteAtomFeeds(@QueryParam("ro") URI roUri, @QueryParam("from") Date from,
-            @QueryParam("to") Date to, @QueryParam("source") URI source, @QueryParam("limit") Integer limit) {
-        AtomFeedEntryDAO entryDAO = new AtomFeedEntryDAO();
-        for (Notification entry : entryDAO.find(roUri, from, to, source, limit)) {
-            entryDAO.delete(entry);
-        }
-        return Response.ok().build();
-    }
+	/**
+	 * Delete Atom feed entries.
+	 * 
+	 * @param roUri
+	 *            Research Object uri
+	 * @param from
+	 *            time - from
+	 * @param to
+	 *            time - to
+	 * @param source
+	 *            feed field source
+	 * @param limit
+	 *            max number of returned entry
+	 * @return Atom Feed with the list of requested entrires.
+	 */
+	@DELETE
+	public Response deleteAtomFeeds(@QueryParam("ro") URI roUri, @QueryParam("from") Date from,
+			@QueryParam("to") Date to, @QueryParam("source") URI source,
+			@QueryParam("limit") Integer limit) {
+		AtomFeedEntryDAO entryDAO = new AtomFeedEntryDAO();
+		for (Notification entry : entryDAO.find(roUri, from, to, source, limit)) {
+			entryDAO.delete(entry);
+		}
+		return Response.ok().build();
+	}
 }
